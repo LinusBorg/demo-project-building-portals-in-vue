3e40609bff9c56149a42e4e7140fabf0b9ecddbf
Thorsten Lünborg <t.luenborg@googlemail.com>
2019-05-05 21:12:45 +0200

    edited Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>Home</div>
+    <p>
+        This is a demo project for a lightning talk about building portals
+        <br>
+        Select one of the Links above to play.
+        <br>
+        Go to
+        <a href="https://github.com/LinusBorg/demo-project-building-portals-in-vue/">the repo</a>
+        to check out the code
+    </p>
 </template>
 <script>
 export default {
